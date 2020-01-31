f4ec08f0c3c0bffe3ee6ed1aad14db5730961345
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-01-30 20:31:19 -0500

    Create LongestCommonSubsequence.java

---
+++ Data Structures and Algorithms/gfgdsa/dynamicprogramming/LongestCommonSubsequence.java
@@ -0,0 +1,53 @@
+/*Longest Common Subsequence
+ * Given two sequences, find the length of longest subsequence present in both of them. Both the strings are of uppercase.
+
+Input:
+First line of the input contains no of test cases  T,the T test cases follow.
+Each test case consist of 2 space separated integers A and B denoting the size of string str1 and str2 respectively
+The next two lines contains the 2 string str1 and str2 .
+
+Output:
+For each test case print the length of longest  common subsequence of the two strings .
+
+Constraints:
+1<=T<=200
+1<=size(str1),size(str2)<=100
+
+Example:
+Input:
+2
+6 6
+ABCDGH
+AEDFHR
+3 2
+ABC
+AC
+
+Output:
+3
+2
+
+Explanation
+LCS for input Sequences “ABCDGH” and “AEDFHR” is “ADH” of length 3.
+
+LCS of "ABC" and "AC" is "AC" of length 2.
+ */package gfgdsa.dynamicprogramming;
+
+public class LongestCommonSubsequence {
+	static int lcs(int p, int q, String s1, String s2) {
+		// your code here
+		int dp[][] = new int[p + 1][q + 1];
+		for (int i = 1; i <= p; i++) {
+			for (int j = 1; j <= q; j++) {
+				if (s1.charAt(i - 1) == s2.charAt(j - 1))
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				else
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+			}
+
+		}
+
+		return dp[p][q];
+	}
+
+}
